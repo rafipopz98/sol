827ef62346e4e5b13173498240c728618ce87ab3
rafipopz98 <itsrealrafi@gmail.com>
2024-06-20 20:28:23 +0530

    arrays

--- contracts/MyContract.sol
+++ contracts/MyContract.sol
@@ -27,4 +27,16 @@ contract MyContract{
 
     UserDetail public  userDetail = UserDetail(1234543,"rafi",21,"nigeria");
 
+    // Arrays
+    uint[] public  myArray = [3,2,1];
+    string[] public newArr;
+    string[] public stringArray=["ahaha","skfhdask","akjdl"];
+    string[][] public my2darray = [["3","45","1"],["2","55","0"]];
+
+    function getLenght()public  view  returns (uint){
+        return myArray.length;
+    }
+    function addvalue(string memory _value) public {
+        newArr.push(_value);
+    }
 }
